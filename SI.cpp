0b112e99ed972f4a4669c3376af233914a9d2588
priyankanaidu <npriyanka783@gmail.com>
2024-05-22 23:48:46 +0530

    SI Fix added runner code extension

--- SI.cpp
+++ SI.cpp
@@ -17,3 +17,32 @@ int main()
 
     return 0;
 }
+
+// C++ program to demonstrate the 
+// cin object 
+// #include <iostream> 
+// using namespace std; 
+  
+// // Driver Code 
+// int main() 
+// { 
+//     int s; 
+//     // Take input using cin 
+//     cin >> s; 
+  
+//     // Print output 
+//     cout << s; 
+  
+//     return 0; 
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   int x;
+//   cout << "Type a number: "; // Type a number and press enter
+//   cin >> x; // Get user input from the keyboard
+//   cout << "Your number is: " << x;
+//   return 0;
+// }
