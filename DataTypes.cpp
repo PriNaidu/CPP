d5fe8e7be211c855f4c4797556620163074a7a83
priyankanaidu <npriyanka783@gmail.com>
2024-05-24 15:56:30 +0530

    [.]

--- DataTypes.cpp
+++ DataTypes.cpp
@@ -1,16 +1,27 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main() {
+int main()
+{
 
-    cout << "Size of char : " << sizeof(char) << endl;
-    cout << "Size of int : " << sizeof(int) << endl;
- 
-    cout << "Size of long : " << sizeof(long) << endl;
-    cout << "Size of float : " << sizeof(float) << endl;
- 
-    cout << "Size of double : " << sizeof(double) << endl;
-    cout << "Size of void : " << sizeof(void) << endl;
-    cout << "Size of wide character :" << sizeof(wchar_t) << endl;
+    // cout << "Size of char : " << sizeof(char) << endl;
+    // cout << "Size of int : " << sizeof(int) << endl;
 
+    // cout << "Size of long : " << sizeof(long) << endl;
+    // cout << "Size of float : " << sizeof(float) << endl;
+
+    // cout << "Size of double : " << sizeof(double) << endl;
+    // cout << "Size of void : " << sizeof(void) << endl;
+    // cout << "Size of wide character :" << sizeof(wchar_t) << endl;
+
+    int a = 123;
+    cout << a << endl;
+    char b = 'v';
+    cout << b << endl;
+
+    bool bl = true;
+    cout << bl << endl;
+
+    bool vl = false;
+    cout<<vl<< endl;
     return 0;
 }
